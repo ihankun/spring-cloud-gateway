a0b2069655d92db6bb1e7aafd332fb424ccbaf6c
hankun <ihankun@outlook.com>
2022-08-27 14:59:27 +0800

    初始化结构

---
+++ src/main/java/com/hankun/parent/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hankun.parent.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
