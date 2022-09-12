26953f1e29abd5ef9fb94ebbcc8234f845b15f3a
hankun <ihankun@outlook.com>
2022-09-12 18:47:42 +0800

    初始化结构

--- src/main/java/com/hankun/parent/gateway/GatewayApplication.java
+++ src/main/java/com/hankun/parent/gateway/GatewayApplication.java
@@ -2,8 +2,15 @@ package com.hankun.parent.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * @author hankun
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackageClasses = GatewayApplication.class)
 public class GatewayApplication {
 
     public static void main(String[] args) {
